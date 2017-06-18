dfc03012f66a3f60ec0398e005f5ad7bc23f3278
Bruno Caruso <bruno@brunocaruso.fr>
2017-06-18 21:36:42 +0200

    Ajout Exemples Interface, DbDate et Requêtes préparées

--- src/fr/marseille/initiation/Interface/Collaborateur.java
+++ src/fr/marseille/initiation/Interface/Collaborateur.java
@@ -1,5 +1,8 @@
 package fr.marseille.initiation.Interface;
 
 public interface Collaborateur {
-
+	public abstract void faireReunion();
+	public abstract void prospecter();
+	public abstract void telephoner();
+	public abstract void distribuerProspectus();
 }
